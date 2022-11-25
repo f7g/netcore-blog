25cb67407c4c7201d328a05615fbc8b698db0709
Fikri Ghazi <fikrigha@gmail.com>
2022-11-25 10:23:49 -0500

    Edit and remove post

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,25 +15,38 @@ public class HomeController : Controller {
 
     [HttpGet]
     public IActionResult Index() {
-        return View();
+        var posts = _repo.GetAllPosts();
+        return View(posts);
     }
 
     [HttpGet]
-    public IActionResult Post() {
-        return View();
+    public IActionResult Post(int id) {
+        var post = _repo.GetPost(id);
+        return View(post);
     }
 
     [HttpGet]
-    public IActionResult Edit() {
-        return View(new Post());
+    public IActionResult Edit(int? id) {
+        if (id == null) return View(new Post());
+        else {
+            var post = _repo.GetPost((int) id);
+            return View(post);
+        }
     }
 
     [HttpPost]
     public async Task<IActionResult> Edit(Post post) {
-        _repo.AddPost(post);
-        if (await _repo.SaveChangesAsync())
-            return RedirectToAction("Index");
-        else
-            return View(post);
+        if (post.Id > 0) _repo.UpdatePost(post);
+        else _repo.AddPost(post);
+        
+        if (await _repo.SaveChangesAsync()) return RedirectToAction("Index");
+        else return View(post);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Remove(int id) {
+        _repo.RemovePost(id);
+        await _repo.SaveChangesAsync();
+        return RedirectToAction("Index");
     }
 }
