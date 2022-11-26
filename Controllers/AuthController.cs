12f6bbe440fae09e5cd63186b25020b5f5fea3b0
Fikri Ghazi <fikrigha@gmail.com>
2022-11-26 18:21:32 -0500

    Add signin and sign out

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+using Blog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+public class AuthController : Controller {
+    // Global variabels
+    private SignInManager<IdentityUser> _signInManager;
+
+    // Constructor
+    public AuthController(SignInManager<IdentityUser> signInManager) {
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult SignIn() {
+        return View(new SignInViewModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SignIn(SignInViewModel signInViewModel) {
+        await _signInManager.PasswordSignInAsync(signInViewModel.Username, signInViewModel.Password, false, false);
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SignOut() {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ ViewModels/SignInViewModel.cs
@@ -0,0 +1,6 @@
+namespace Blog.ViewModels;
+
+public class SignInViewModel {
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
