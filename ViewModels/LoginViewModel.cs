deae918193eab60fc603ad9fff33efed4f5bd6f8
Fikri Ghazi <fikrigha@gmail.com>
2022-11-27 12:59:12 -0500

    Update styling

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 namespace Blog.ViewModels;
 
 public class LoginViewModel {
     public string Username { get; set; } = null!;
+    [DataType(DataType.Password)]
     public string Password { get; set; } = null!;
 }
